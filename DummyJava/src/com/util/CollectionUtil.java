08c0be175f1f0dc3ff36657d3de43e898578586c
Likisee <likisee@gmail.com>
2018-08-19 02:38:38 +0800

    add getArrayList2Array, getArrayList2HashMap

--- DummyJava/src/com/util/CollectionUtil.java
+++ DummyJava/src/com/util/CollectionUtil.java
@@ -2,6 +2,7 @@ package com.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,7 +22,7 @@ public class CollectionUtil {
 			Collections.sort(dataList);
 		}
 	}
-	
+
 	public static void getIntegerArrayListSortWithoutDuplication(ArrayList<Integer> dataList) {
 		if (dataList != null) {
 			Set<Integer> hs = new HashSet<Integer>();
@@ -30,6 +31,28 @@ public class CollectionUtil {
 			dataList.addAll(hs);
 			Collections.sort(dataList);
 		}
+	}
+
+	public static Integer[] getArrayList2Array(ArrayList<Integer> dataArrayList) {
+		Integer[] dataArray = new Integer[dataArrayList.size()];
+		dataArrayList.toArray(dataArray);
+		return dataArray;
+	}
+	
+	public static Integer[][] getArrayList2Array(ArrayList<ArrayList<Integer>> dataArrayList) {
+		Integer[][] dataArray = new Integer[dataArrayList.size()][dataArrayList.get(0).size()];
+		for(int i = 0; i < dataArrayList.size(); i++) {
+			dataArrayList.get(i).toArray(dataArray[i]);	
+		}
+		return dataArray;
+	}
+
+	public static HashMap<Integer, Integer> getArrayList2HashMap(ArrayList<Integer> dataArrayList) {
+		HashMap<Integer, Integer> dataHashMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < dataArrayList.size(); i++) {
+			dataHashMap.put(dataArrayList.get(i), i);
+		}
+		return dataHashMap;
 	}
 
 	public static void main(String[] args) {
